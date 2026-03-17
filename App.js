6925ed13cb2f1bc41ef85735f1bd3b5c5ec5bf5b
Lucas Negreiro <negreiro.lucas@hotmail.com>
2026-03-17 20:06:09 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import Exemplo01 from './src/exemplos/ex01';
+import Exemplo02 from './src/exemplos/ex02';
+
+import Atividade01 from './src/atividades/atv01';
+import Atividade02 from './src/atividades/atv02';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Atividade02 />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00290a', 
+    padding: 16, 
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  }, 
+});
